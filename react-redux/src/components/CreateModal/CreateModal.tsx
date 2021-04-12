167b236b59f4e08fd0b46cfc66e73271108f8bff
Katsiaryna Staurova <Katsiaryna_Staurova@epam.com>
2021-04-12 20:54:39 +0300

    feat: create form using formik

--- react-redux/src/components/CreateModal/CreateModal.tsx
+++ react-redux/src/components/CreateModal/CreateModal.tsx
@@ -1,75 +1,105 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Formik, Field, Form } from 'formik';
 import { Links } from '../../../mocksData/linkItems';
-import { InitialMovie } from '../../../mocksData/initialMovie';
+import { InitialCreateMovie } from '../../../mocksData/initialCreateMovie';
 import './CreateModal.css';
 
 export default function CreateModal(props) {
   const { show, handleConfirm } = props;
+  const formData = InitialCreateMovie;
 
   if (!show) {
     return null;
   }
 
-  const [currentMovie, setMovie] = useState(InitialMovie);
+  const onChangeGenres = (event, setFieldValue) => {
+    setFieldValue('genres', event.target.value);
+  };
 
   return (
     <div className="modal-edit" id="modal-edit">
-      <div className="modal-edit-wrap">
-        <div className="edit-header">
-          <span className="edit-close-sign" onClick={props.onClose}>x</span>
-        </div>
-        <div className="edit-body">
-          <h2>Add movie</h2>
-          <label htmlFor="movie-title">Title</label>
-          <input
-            className="edit-input"
-            id='movie-title'
-            type='text'
-            value={currentMovie.title}
-            placeholder='e.g. Moana'
-          />
-          <label htmlFor="release-date">Release Date</label>
-          <input
-            className="edit-input"
-            id='release-date'
-            type='date'
-            value={new Date(currentMovie.release_date).getFullYear()}
-            placeholder='Select Date'
-          />
-          <label htmlFor="movie-url">Movie URL</label>
-          <input
-            className="edit-input"
-            id='movie-url'
-            type='url'
-            placeholder='e.g. moana.com'
-          />
-          <select className="edit-input filter-select">
-            {
-              Links.map((el, index) => <option key={el} value={index + 1}>{el}</option>)
-            }
-          </select>
-          <label htmlFor="overview">Overview</label>
-          <input
-            className="edit-input"
-            id='overview'
-            type='text'
-            placeholder='Type movie overview here'
-          />
-          <label htmlFor="runtime">Runtime</label>
-          <input
-            className="edit-input"
-            id='runtime'
-            name='runtime'
-            type='text'
-            placeholder='Movie runtime'
-          />
-        </div>
-        <div className="edit-footer">
-          <button className='edit-reset-btn' type='reset' onClick={() => setMovie(InitialMovie)}>Reset</button>
-          <button className='edit-submit-btn' type='submit' onClick={handleConfirm}>Save</button>
-        </div>
-      </div>
+      <Formik
+          initialValues={formData}
+          onSubmit={(values) => handleConfirm(values)}
+        >
+         {({ setFieldValue }) => {
+           return (
+            <Form className="modal-edit-wrap">
+                <div className="edit-header">
+                  <span className="edit-close-sign" onClick={props.onClose}>x</span>
+                </div>
+                <div className="edit-body">
+                  <h2>Add movie</h2>
+                  <label htmlFor="movie-title">Title</label>
+                  <Field
+                    className="edit-input"
+                    id='movie-title'
+                    type='text'
+                    name='title'
+                    placeholder='e.g. Moana'
+                    autoComplete="off"
+                  />
+                  <label htmlFor="release-date">Release Date</label>
+                  <Field
+                    className="edit-input"
+                    id='release-date'
+                    type='date'
+                    name="release_date"
+                    placeholder='Select Date'
+                  />
+                  <label htmlFor="movie-url">Movie URL</label>
+                  <Field
+                    className="edit-input"
+                    id='movie-url'
+                    type='url'
+                    name='poster_path'
+                    placeholder='e.g. moana.com'
+                    autoComplete="off"
+                  />
+                  <label htmlFor="genres">Genre</label>
+                    <Field id="genres" className="edit-input filter-select" name="genres" component="select" onChange={(e: { target: { value: any; }; }) => onChangeGenres(e, setFieldValue)}>
+                      <option hidden value=""></option>
+                      {
+                        Links.map((el, index) => <option key={el} value={index + 1}>{el}</option>)
+                      }
+                    </Field>
+                  <label htmlFor="overview">Overview</label>
+                  <Field
+                    className="edit-input"
+                    id='overview'
+                    type='text'
+                    name='overview'
+                    placeholder='Type movie overview here'
+                    autoComplete="off"
+                  />
+                  <label htmlFor="runtime">Runtime</label>
+                  <Field
+                    className="edit-input"
+                    id='runtime'
+                    name='runtime'
+                    type='text'
+                    placeholder='Movie runtime'
+                    autoComplete="off"
+                  />
+                  <label htmlFor="tagline">Tagline</label>
+                  <Field
+                    className="edit-input"
+                    id='tagline'
+                    name='tagline'
+                    type='text'
+                    placeholder='Movie tagline'
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="edit-footer">
+                  <button className='edit-reset-btn' type='reset'>Reset</button>
+                  <button className='edit-submit-btn' type='submit'>Save</button>
+                </div>
+            </Form>
+          );
+        }}
+      </Formik>
     </div>
   );
 }
--- react-redux/src/containers/Header/components/AddMovieButton/AddMovieButton.tsx
+++ react-redux/src/containers/Header/components/AddMovieButton/AddMovieButton.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import CreateModal from '../../../../components/CreateModal';
 import { useDispatch } from 'react-redux';
 import { createMovie } from '../../../../actions';
+import { Links } from '../../../../../mocksData/linkItems';
 import './AddMovieButton.css';
 
 export default function AddMovieButton() {
@@ -18,25 +19,20 @@ export default function AddMovieButton() {
     setShowAddModal(true);
   }
 
-  const handleConfirm = (event) => {
-    event.preventDefault();
+  const handleConfirm = (values) => {
     const movie = {
-      "title": "My best friend",
-      "tagline": "Here's to the fools who dream.",
-      "vote_average": 7.9,
-      "vote_count": 6782,
-      "release_date": "2016-12-29",
-      "poster_path": "https://image.tmdb.org/t/p/w500/ylXCdC106IKiarftHkcacasaAcb.jpg",
-      "overview": "Mia, an aspiring actress, serves lattes to movie stars in between auditions and Sebastian, a jazz musician, scrapes by playing cocktail party gigs in dingy bars, but as success mounts they are faced with decisions that begin to fray the fragile fabric of their love affair, and the dreams they worked so hard to maintain in each other threaten to rip them apart.",
-      "budget": 30000000,
-      "revenue": 445435700,
-      "runtime": 128,
-      "genres": [
-        "Comedy",
-        "Drama",
-        "Romance"
-      ]
+      title: values.title,
+      tagline: values.tagline,
+      vote_average: 0,
+      vote_count: 0,
+      release_date: values.release_date,
+      poster_path: values.poster_path,
+      overview: values.overview,
+      budget: +values.budget,
+      runtime: +values.runtime,
+      genres: Links[values.genres] === 'All' ? Links : [Links[values.genres]]
     }
+
     dispatch(createMovie(movie));
     setShowAddModal(false);
   }
